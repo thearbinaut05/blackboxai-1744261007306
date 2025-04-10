94aa5e0c7d95a1f33781831d5c1b967b1d30d404
thearbinaut05 <thearbinaut05@gmail.com>
2025-04-09 21:57:39 -0700

    Add defi-protocol/hardhat.config.js 1744261056672

---
+++ defi-protocol/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  mocha: {
+    timeout: 100000
+  }
+};
